088f128de65631b05bd081f40688a1fe97cc3477
Andrew Yatsura <andrewyazura203@proton.me>
2024-03-21 12:00:47 +0100

    add current configuration

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "NixOS system-wide configuration";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+
+    home-manager = {
+      url = "github:nix-community/home-manager/release-23.11";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = inputs@{ nixpkgs, home-manager, ... }: {
+    nixosConfigurations = {
+      nixos = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./configuration.nix
+
+          home-manager.nixosModules.home-manager {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+
+            home-manager.users.andrew = import ./home.nix;
+          }
+        ];
+      };
+    };
+  };
+}
