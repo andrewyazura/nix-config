659dcf519b9f2e12d02cd15493f3e05d0fa62c57
Andrew Yatsura <andrewyazura203@proton.me>
2024-03-28 03:06:07 +0100

    generation: 38 - 2024-03-28 03:06:04

--- home/i3/default.nix
+++ home/i3/default.nix
@@ -1,4 +1,9 @@
 { pkgs, config, ... }: {
   home.file.".config/i3/config".source = ./config;
   home.file.".config/i3/wallpaper.jpg".source = ../../wallpaper.jpg;
+  home.file.".config/i3/scripts" = {
+    source = ./scripts;
+    recursive = true;
+    executable = true;
+  };
 }
