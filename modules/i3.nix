aa20f152f7fbef38b2a9cc473537b93662f38a82
Andrew Yatsura <andrewyazura203@proton.me>
2024-03-28 01:43:37 +0100

    generation: 37 - 2024-03-28 01:43:34

--- modules/i3.nix
+++ modules/i3.nix
@@ -13,7 +13,7 @@
       enable = true;
       extraPackages = with pkgs; [ dmenu i3status i3lock feh dunst acpi ];
       extraSessionCommands = ''
-        eval $(gnome-keyring-daemon --daemonize)
+        eval $(gnome-keyring-daemon --start)
         export GNOME_KEYRING_CONTROL GNOME_KEYRING_PID GPG_AGENT_INFO SSH_AUTH_SOCK
       '';
     };
