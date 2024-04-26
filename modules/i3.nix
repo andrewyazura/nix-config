08851656481b85182089306ecdb7094fed440b96
Andrew Yatsura <andrewyazura203@proton.me>
2024-04-26 22:41:01 +0200

    generation: 111 - 2024-04-26 22:39:51

--- modules/i3.nix
+++ modules/i3.nix
@@ -4,25 +4,28 @@
   services.gnome.gnome-keyring.enable = true;
   services.picom.enable = true;
 
-  services.xserver = {
-    enable = true;
-
-    desktopManager = { xterm.enable = false; };
+  services = {
     displayManager = { defaultSession = "none+i3"; };
 
-    windowManager.i3 = {
+    xserver = {
       enable = true;
-      extraPackages = with pkgs; [ rofi polybar i3lock-color feh dunst acpi ];
-      extraSessionCommands = ''
-        eval $(gnome-keyring-daemon --start)
-        export GNOME_KEYRING_CONTROL GNOME_KEYRING_PID GPG_AGENT_INFO SSH_AUTH_SOCK
-      '';
-    };
 
-    xkb = {
-      layout = "us,ua";
-      variant = "";
-      options = "grp:win_space_toggle";
+      desktopManager = { xterm.enable = false; };
+
+      windowManager.i3 = {
+        enable = true;
+        extraPackages = with pkgs; [ rofi polybar i3lock-color feh dunst acpi ];
+        extraSessionCommands = ''
+          eval $(gnome-keyring-daemon --start)
+          export GNOME_KEYRING_CONTROL GNOME_KEYRING_PID GPG_AGENT_INFO SSH_AUTH_SOCK
+        '';
+      };
+
+      xkb = {
+        layout = "us,ua";
+        variant = "";
+        options = "grp:win_space_toggle";
+      };
     };
   };
 }
