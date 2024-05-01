b66d2ead2b230173de0c291c35aafaa5342c7a41
Andrew Yatsura <andrewyazura203@proton.me>
2024-05-01 03:29:42 +0200

    generation: 163 - 2024-05-01 03:29:38

--- modules/hyprland.nix
+++ modules/hyprland.nix
@@ -16,7 +16,6 @@
   programs.hyprland = {
     enable = true;
     xwayland.enable = true;
-    package = inputs.hyprland.packages."${pkgs.system}".hyprland;
   };
 
   environment.systemPackages = with pkgs; [
