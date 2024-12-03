9039513ea8212fe86fe26f1f1294c958e28728e9
Andrew Yatsura <andrewyazura203@proton.me>
2024-12-03 05:37:22 +0100

    add hyprland plugins and fix inputs

--- flake.nix
+++ flake.nix
@@ -38,7 +38,7 @@
           home-manager.nixosModules.home-manager {
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
-            home-manager.extraSpecialArgs = { inherit username; };
+            home-manager.extraSpecialArgs = inputs // { inherit username; };
             home-manager.users.${username} = import ./users/${username}/home.nix;
           }
         ];
--- home/hyprland/default.nix
+++ home/hyprland/default.nix
@@ -1,15 +1,20 @@
-{ pkgs, ... }: {
+{ pkgs, hyprland, hyprland-plugins, ... }: {
   programs.kitty.enable = true;
   wayland.windowManager.hyprland = let
     system = pkgs.stdenv.hostPlatform.system;
   in {
     enable = true;
-    # package = inputs.hyprland.packages.${system}.hyprland;
-    # plugins = [
-    #   inputs.hyprland-plugins.packages.${system}.borders-plus-plus
-    #   inputs.hyprland-plugins.packages.${system}.hy3
-    #   inputs.hyprland-plugins.packages.${system}.hyprbars
-    #   inputs.hyprland-plugins.packages.${system}.hyprtrails
-    # ];
+    package = hyprland.packages.${system}.hyprland;
+    plugins = with hyprland-plugins; [
+      packages.${system}.borders-plus-plus
+      packages.${system}.hyprbars
+      packages.${system}.hyprtrails
+    ];
+    settings = {
+      "$mod" = "SUPER";
+      bind = [
+        "$mod, F, exec, firefox"
+      ];
+    };
   };
 }
