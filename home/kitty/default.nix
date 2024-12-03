a8c8f5a6b7abe1b2e61579a13566ce4bb4cd8767
Andrew Yatsura <andrewyazura203@proton.me>
2024-12-03 07:40:37 +0100

    fix kitty

--- home/kitty/default.nix
+++ home/kitty/default.nix
@@ -1,3 +1,4 @@
 { ... }: {
-  programs.kitty.enable = true;
+  home.file.".config/kitty/kitty.conf".source = ./kitty.conf;
+  home.file.".config/kitty/current-theme.conf".source = ./current-theme.conf;
 }
