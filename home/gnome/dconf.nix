4761f890c568fa43ca3e396050cde5318feb96b0
Andrew Yatsura <andrewyazura203@proton.me>
2025-01-29 13:18:13 +0100

    update gnome config

--- home/gnome/dconf.nix
+++ home/gnome/dconf.nix
@@ -5,9 +5,55 @@ with lib.hm.gvariant;
 
 {
   dconf.settings = {
+    "org/gnome/Console" = {
+      last-window-maximised = false;
+      last-window-size = mkTuple [ 1276 480 ];
+    };
+
+    "org/gnome/Extensions" = {
+      window-height = 1027;
+      window-width = 725;
+    };
+
+    "org/gnome/TextEditor" = {
+      last-save-directory = "file:///home/andrew/Downloads";
+    };
+
     "org/gnome/control-center" = {
-      last-panel = "wifi";
+      last-panel = "keyboard";
       window-state = mkTuple [ 980 640 false ];
+    };
+
+    "org/gnome/desktop/app-folders" = {
+      folder-children = [ "Utilities" "YaST" "Pardus" ];
+    };
+
+    "org/gnome/desktop/app-folders/folders/Pardus" = {
+      categories = [ "X-Pardus-Apps" ];
+      name = "X-Pardus-Apps.directory";
+      translate = true;
+    };
+
+    "org/gnome/desktop/app-folders/folders/Utilities" = {
+      apps = [ "org.freedesktop.GnomeAbrt.desktop" "nm-connection-editor.desktop" "org.gnome.baobab.desktop" "org.gnome.Connections.desktop" "org.gnome.DejaDup.desktop" "org.gnome.DiskUtility.desktop" "org.gnome.Evince.desktop" "org.gnome.FileRoller.desktop" "org.gnome.font-viewer.desktop" "org.gnome.Loupe.desktop" "org.gnome.seahorse.Application.desktop" "org.gnome.tweaks.desktop" "org.gnome.Usage.desktop" ];
+      categories = [ "X-GNOME-Utilities" ];
+      name = "X-GNOME-Utilities.directory";
+      translate = true;
+    };
+
+    "org/gnome/desktop/app-folders/folders/YaST" = {
+      categories = [ "X-SuSE-YaST" ];
+      name = "suse-yast.directory";
+      translate = true;
+    };
+
+    "org/gnome/desktop/background" = {
+      color-shading-type = "solid";
+      picture-options = "zoom";
+      picture-uri = "file:///run/current-system/sw/share/backgrounds/gnome/amber-l.jxl";
+      picture-uri-dark = "file:///run/current-system/sw/share/backgrounds/gnome/amber-d.jxl";
+      primary-color = "#ff7800";
+      secondary-color = "#000000";
     };
 
     "org/gnome/desktop/calendar" = {
@@ -19,18 +65,42 @@ with lib.hm.gvariant;
     };
 
     "org/gnome/desktop/input-sources" = {
+      mru-sources = [ (mkTuple [ "xkb" "us" ]) (mkTuple [ "xkb" "ua" ]) ];
       sources = [ (mkTuple [ "xkb" "us" ]) (mkTuple [ "xkb" "ua" ]) ];
+      xkb-options = [ "terminate:ctrl_alt_bksp" ];
     };
 
     "org/gnome/desktop/interface" = {
       clock-show-seconds = false;
       clock-show-weekday = true;
-      color-scheme = "prefer-dark";
+      color-scheme = "default";
+      enable-hot-corners = false;
       toolkit-accessibility = false;
     };
 
     "org/gnome/desktop/notifications" = {
+      application-children = [ "gnome-power-panel" "steam" "vesktop" "firefox" "org-telegram-desktop" ];
       show-in-lock-screen = false;
+    };
+
+    "org/gnome/desktop/notifications/application/firefox" = {
+      application-id = "firefox.desktop";
+    };
+
+    "org/gnome/desktop/notifications/application/gnome-power-panel" = {
+      application-id = "gnome-power-panel.desktop";
+    };
+
+    "org/gnome/desktop/notifications/application/org-telegram-desktop" = {
+      application-id = "org.telegram.desktop.desktop";
+    };
+
+    "org/gnome/desktop/notifications/application/steam" = {
+      application-id = "steam.desktop";
+    };
+
+    "org/gnome/desktop/notifications/application/vesktop" = {
+      application-id = "vesktop.desktop";
     };
 
     "org/gnome/desktop/peripherals/mouse" = {
@@ -41,20 +111,128 @@ with lib.hm.gvariant;
       two-finger-scrolling-enabled = true;
     };
 
+    "org/gnome/desktop/screensaver" = {
+      color-shading-type = "solid";
+      picture-options = "zoom";
+      picture-uri = "file:///run/current-system/sw/share/backgrounds/gnome/amber-l.jxl";
+      primary-color = "#ff7800";
+      secondary-color = "#000000";
+    };
+
     "org/gnome/desktop/search-providers" = {
       sort-order = [ "org.gnome.Settings.desktop" "org.gnome.Contacts.desktop" "org.gnome.Nautilus.desktop" ];
     };
 
+    "org/gnome/desktop/wm/keybindings" = {
+      close = [ "<Super>q" ];
+    };
+
+    "org/gnome/evolution-data-server" = {
+      migrated = true;
+    };
+
+    "org/gnome/gnome-system-monitor" = {
+      current-tab = "processes";
+      show-dependencies = false;
+      show-whose-processes = "user";
+      window-width = 1224;
+    };
+
+    "org/gnome/gnome-system-monitor/proctree" = {
+      col-0-visible = true;
+      col-0-width = 305;
+      col-26-visible = false;
+      col-26-width = 0;
+      columns-order = [ 0 12 1 2 3 4 6 7 8 9 10 11 13 14 15 16 17 18 19 20 21 22 23 24 25 26 ];
+      sort-col = 0;
+      sort-order = 0;
+    };
+
+    "org/gnome/mutter" = {
+      dynamic-workspaces = true;
+      edge-tiling = false;
+    };
+
     "org/gnome/nautilus/preferences" = {
+      default-folder-viewer = "icon-view";
       migrated-gtk-settings = true;
+      search-filter-time-type = "last_modified";
+    };
+
+    "org/gnome/nautilus/window-state" = {
+      initial-size = mkTuple [ 890 550 ];
+    };
+
+    "org/gnome/nm-applet/eap/6c909423-197f-4862-9ab1-a4c771e2ffab" = {
+      ignore-ca-cert = false;
+      ignore-phase2-ca-cert = false;
+    };
+
+    "org/gnome/nm-applet/eap/ca442079-d825-48b9-aedf-2eea2b3131f8" = {
+      ignore-ca-cert = false;
+      ignore-phase2-ca-cert = false;
     };
 
     "org/gnome/settings-daemon/plugins/color" = {
       night-light-schedule-automatic = false;
     };
 
+    "org/gnome/settings-daemon/plugins/media-keys" = {
+      custom-keybindings = [ "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/" ];
+      screensaver = [ "<Shift><Super>Escape" ];
+    };
+
+    "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0" = {
+      binding = "<Super>t";
+      command = "ghostty";
+      name = "Terminal";
+    };
+
+    "org/gnome/settings-daemon/plugins/power" = {
+      power-button-action = "nothing";
+    };
+
+    "org/gnome/shell" = {
+      disable-user-extensions = true;
+      favorite-apps = [ "org.gnome.Nautilus.desktop" ];
+      last-selected-power-profile = "performance";
+      welcome-dialog-last-shown-version = "47.2";
+    };
+
+    "org/gnome/shell/app-switcher" = {
+      current-workspace-only = true;
+    };
+
     "org/gnome/shell/world-clocks" = {
       locations = [];
+    };
+
+    "org/gtk/gtk4/settings/file-chooser" = {
+      date-format = "regular";
+      location-mode = "path-bar";
+      show-hidden = true;
+      sidebar-width = 140;
+      sort-column = "name";
+      sort-directories-first = true;
+      sort-order = "ascending";
+      type-format = "category";
+      view-type = "list";
+      window-size = mkTuple [ 1178 372 ];
+    };
+
+    "org/gtk/settings/file-chooser" = {
+      date-format = "regular";
+      location-mode = "path-bar";
+      show-hidden = false;
+      show-size-column = true;
+      show-type-column = true;
+      sidebar-width = 157;
+      sort-column = "name";
+      sort-directories-first = false;
+      sort-order = "ascending";
+      type-format = "category";
+      window-position = mkTuple [ 26 23 ];
+      window-size = mkTuple [ 1203 902 ];
     };
 
   };
