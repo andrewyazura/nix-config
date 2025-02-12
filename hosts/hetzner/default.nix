1809d33695e022765df45b28dbef87cf4d8237e1
Andrew Yatsura <andrewyazura203@proton.me>
2025-02-12 10:36:24 +0100

    minor refactoring of hetzner config

--- hosts/hetzner/default.nix
+++ hosts/hetzner/default.nix
@@ -4,6 +4,7 @@
   nix.settings = {
     experimental-features = "nix-command flakes";
     auto-optimise-store = true;
+    trusted-users = [ "@wheel" ];
   };
 
   environment.systemPackages = with pkgs; [ vim git ];
@@ -22,12 +23,13 @@
   time.timeZone = "Europe/London";
   i18n.defaultLocale = "en_US.UTF-8";
   console.keyMap = "us";
-  nix.settings.trusted-users = [ "@wheel" ];
 
-  boot.loader.grub.enable = true;
-  boot.loader.grub.device = "/dev/sda";
-  boot.initrd.availableKernelModules =
-    [ "ahci" "xhci_pci" "virtio_pci" "virtio_scsi" "sd_mod" "sr_mod" "ext4" ];
+  boot = {
+    loader.grub.enable = true;
+    loader.grub.device = "/dev/sda";
+    initrd.availableKernelModules =
+      [ "ahci" "xhci_pci" "virtio_pci" "virtio_scsi" "sd_mod" "sr_mod" "ext4" ];
+  };
 
   users.users = {
     root.hashedPassword = "!";
@@ -40,12 +42,14 @@
 
   security.sudo.wheelNeedsPassword = false;
 
-  services.openssh = {
-    enable = true;
-    settings = {
-      PermitRootLogin = "no";
-      PasswordAuthentication = false;
-      KbdInteractiveAuthentication = false;
+  services = {
+    openssh = {
+      enable = true;
+      settings = {
+        PermitRootLogin = "no";
+        PasswordAuthentication = false;
+        KbdInteractiveAuthentication = false;
+      };
     };
   };
   networking.firewall.allowedTCPPorts = [ 22 ];
