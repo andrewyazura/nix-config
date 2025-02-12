ac682c96c18039970a426256bea9f6c5b0b0f14a
Andrew Yatsura <andrewyazura203@proton.me>
2025-02-12 10:36:13 +0100

    disable mouse in neovim since there is no hardtime.nvim now

--- home/neovim/configs/options.lua
+++ home/neovim/configs/options.lua
@@ -23,3 +23,10 @@ require("catppuccin").setup({
 })
 
 vim.cmd([[colorscheme catppuccin]])
+
+vim.keymap.set("", "<up>", "<nop>", { noremap = true })
+vim.keymap.set("", "<down>", "<nop>", { noremap = true })
+vim.keymap.set("i", "<up>", "<nop>", { noremap = true })
+vim.keymap.set("i", "<down>", "<nop>", { noremap = true })
+
+vim.opt.mouse = ""
