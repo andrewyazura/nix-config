cfe16a26597f7380df5c88cc2dc27a70aebb547a
Andrew Yatsura <andrewyazura203@proton.me>
2025-02-18 04:20:59 +0100

    add my laptop

--- flake.nix
+++ flake.nix
@@ -4,6 +4,8 @@
   inputs = {
     nixpkgs = { url = "github:nixos/nixpkgs/nixos-unstable"; };
 
+    nixos-hardware = { url = "github:NixOS/nixos-hardware/master"; };
+
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -14,7 +16,7 @@
     ghostty = { url = "github:ghostty-org/ghostty"; };
   };
 
-  outputs = inputs@{ nixpkgs, home-manager, ... }: {
+  outputs = inputs@{ nixpkgs, nixos-hardware, home-manager, ... }: {
     nixosConfigurations = {
       r7-x3d = let username = "andrew";
       in nixpkgs.lib.nixosSystem {
@@ -35,6 +37,26 @@
           inputs.minegrub-theme.nixosModules.default
         ];
         specialArgs = { inherit inputs; };
+      };
+
+      ga401 = let username = "andrew";
+      in nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./hosts/ga401
+          ./users/${username}/system.nix
+          nixos-hardware.nixosModules.asus-zephyrus-ga401
+
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.extraSpecialArgs = inputs  // { inherit username; };
+            home-manager.users.${username} = 
+              import ./users/${username}/home.nix;
+          }
+        ];
+        specialArgs = {inherit inputs;};
       };
 
       hetzner-x86_64 = nixpkgs.lib.nixosSystem {
