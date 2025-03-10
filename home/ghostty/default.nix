b887694c99dfe7eb81bfdbe307bee4270b9a8bbd
Andrew Yatsura <andrewyazura203@proton.me>
2025-03-11 00:34:50 +0100

    change ghostty opacity

--- home/ghostty/default.nix
+++ home/ghostty/default.nix
@@ -2,7 +2,7 @@
   programs.ghostty = {
     enable = true;
     settings = {
-      background-opacity = 0.95;
+      background-opacity = 1;
       font-size = 10;
       font-family = "FiraCode Nerd Font Mono";
       theme = "catppuccin-mocha";
