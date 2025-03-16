73ffc606668537200683afa82b4a9210eb898f33
Andrew Yatsura <andrewyazura203@proton.me>
2025-03-16 12:43:26 +0100

    fix cs2 config

--- home/cs2/default.nix
+++ home/cs2/default.nix
@@ -7,6 +7,6 @@ in {
   config = mkIf cfg.enable {
     home.file.".local/share/Steam/steamapps/common/Counter-Strike Global Offensive/game/csgo/cfg/autoexec.cfg".source =
       ./autoexec.cfg;
-    # LD_PRELOAD="" gamescope -W 3840 -H 2160 -w 1920 -h 1080 -r 144 -S integer --fullscreen --force-grab-cursor --immediate-flips -- %command% -novid -nojoy -refresh 144 -high -forcepreload 1 -threads 9 +mat_disable_fancy_blending 1 -mat_queue_mode 2 +engine_low_latency_sleep_after_client_tick +violence_hblood 0
+    # -refresh 144 -high -fullscreen -forcepreload 1 -threads 9 +mat_disable_fancy_blending 1 -mat_queue_mode 2 +engine_low_latency_sleep_after_client_tick
   };
 }
