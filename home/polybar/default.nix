37a2fb8aa90124adb363f4855ead8625eeaad247
Andrew Yatsura <andrewyazura203@proton.me>
2025-03-25 02:16:02 +0100

    add catppuccin and tweak some modules in polybar

--- home/polybar/default.nix
+++ home/polybar/default.nix
@@ -14,40 +14,57 @@ in {
 
       config = let
         colors = {
-          background = "#111";
-          background-alt = "#444";
-
-          foreground = "#ddd";
-          foreground-alt = "#555";
-
-          primary = "#ffb52a";
-          secondary = "#e60053";
-          alert = "#bd2c40";
-          accent = "#4bc98a";
+          rosewater = "#f5e0dc";
+          flamingo = "#f2cdcd";
+          pink = "#f5c2e7";
+          mauve = "#cba6f7";
+          red = "#f38ba8";
+          maroon = "#eba0ac";
+          peach = "#fab387";
+          yellow = "#f9e2af";
+          green = "#a6e3a1";
+          teal = "#94e2d5";
+          sky = "#89dceb";
+          sapphire = "#74c7ec";
+          blue = "#89b4fa";
+          lavender = "#b4befe";
+          text = "#cdd6f4";
+          subtext1 = "#bac2de";
+          subtext0 = "#a6adc8";
+          overlay2 = "#9399b2";
+          overlay1 = "#7f849c";
+          overlay0 = "#6c7086";
+          surface2 = "#585b70";
+          surface1 = "#45475a";
+          surface0 = "#313244";
+          base = "#1e1e2e";
+          mantle = "#181825";
+          crust = "#11111b";
+          transparent = "#FF00000";
         };
       in {
         "bar/top" = {
           width = "100%";
-          height = 24;
+          height = 16;
 
           radius = 0;
           line-size = 0;
           border-size = 0;
 
-          background = "${colors.background}";
-          foreground = "${colors.foreground}";
+          background = "${colors.base}";
+          foreground = "${colors.text}";
 
           modules-left = "i3";
           modules-center = "";
-          modules-right = "date battery";
+          modules-right = "wifi date battery";
 
-          module-margin-left = 1;
+          module-margin-left = 2;
           module-margin-right = 2;
 
           padding-left = 0;
-          padding-right = 2;
+          padding-right = 0;
 
-          font-0 = "Fira Code:pixelsize=10;0";
+          font-0 = "Fira Code Nerd Font:pixelsize=10;2";
         };
 
         "module/i3" = {
@@ -56,38 +73,38 @@ in {
           index-sort = true;
           wrapping-scroll = false;
 
-          pin-workspaces = false;
+          pin-workspaces = true;
 
-          label-mode-padding = 2;
           label-mode-foreground = "#000";
-          label-mode-background = "${colors.primary}";
+          label-mode-background = "${colors.blue}";
+          label-mode-padding = 1;
 
           label-focused = "%name%";
-          label-focused-background = "${colors.accent}";
-          label-focused-underline = "${colors.background}";
-          label-focused-foreground = "${colors.background}";
-          label-focused-padding = 2;
+          label-focused-background = "${colors.mauve}";
+          label-focused-underline = "${colors.base}";
+          label-focused-foreground = "${colors.base}";
+          label-focused-padding = 1;
 
           label-unfocused = "%name%";
-          label-unfocused-padding = 2;
+          label-unfocused-padding = 1;
 
           label-visible = "%name%";
-          label-visible-background = "${colors.accent}";
-          label-visible-underline = "${colors.background}";
-          label-visible-padding = 2;
+          label-visible-background = "${colors.mauve}";
+          label-visible-underline = "${colors.base}";
+          label-visible-padding = 1;
 
           label-urgent = "%name%";
-          label-urgent-background = "${colors.alert}";
-          label-urgent-underline = "${colors.background}";
-          label-urgent-padding = 2;
+          label-urgent-background = "${colors.red}";
+          label-urgent-underline = "${colors.base}";
+          label-urgent-padding = 1;
         };
 
         "module/date" = {
           type = "internal/date";
           interval = 1;
 
-          date = "%Y-%m-%d";
-          date-alt = "";
+          date = "%A, %B %d";
+          date-alt = "%Y-%m-%d";
 
           time = "%H:%M";
           time-alt = "%H:%M:%S";
@@ -95,14 +112,66 @@ in {
           label = "%date% %time%";
         };
 
-        "module/battery" = {
+        "module/battery" = let label = "%percentage%%";
+        in {
           type = "internal/battery";
+
           full-at = 99;
           low-at = 20;
 
           battery = "BAT0";
           adapter = "AC0";
-          poll-interval = 10;
+
+          format-full = "<ramp-capacity> <label-full>";
+          format-full-background = "${colors.base}";
+          format-full-foreground = "${colors.green}";
+
+          format-charging = "<ramp-capacity> <label-charging>";
+          format-charging-background = "${colors.base}";
+          format-charging-foreground = "${colors.green}";
+
+          format-discharging = "<ramp-capacity> <label-discharging>";
+          format-discharging-background = "${colors.base}";
+          format-discharging-foreground = "${colors.mauve}";
+
+          format-low = "<ramp-capacity> <label-low>";
+          format-low-background = "${colors.base}";
+          format-low-foreground = "${colors.red}";
+
+          label-full = label;
+          label-charging = label;
+          label-discharging = label;
+          label-low = label;
+
+          ramp-capacity-0 = "▁";
+          ramp-capacity-1 = "▂";
+          ramp-capacity-2 = "▃";
+          ramp-capacity-3 = "▅";
+          ramp-capacity-4 = "▇";
+          ramp-capacity-5 = "█";
+        };
+
+        "module/wifi" = {
+          type = "internal/network";
+          interface = "wlp2s0";
+          interface-type = "wireless";
+
+          format-connected = "<ramp-signal> <label-connected>";
+          format-disconnected = "<label-disconnected>";
+
+          label-connected = "%essid% %downspeed:9%";
+          label-connected-foreground = "${colors.mauve}";
+
+          label-disconnected = "not connected";
+          label-disconnected-background = "${colors.red}";
+          label-disconnected-foreground = "${colors.text}";
+
+          ramp-signal-0 = "▁";
+          ramp-signal-1 = "▂";
+          ramp-signal-2 = "▃";
+          ramp-signal-3 = "▅";
+          ramp-signal-4 = "▇";
+          ramp-signal-5 = "█";
         };
       };
     };
