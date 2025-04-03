0bacfef8c8cdead5140d6ef9e432e0e167eb72e6
Andrew Yatsura <andrewyazura203@proton.me>
2025-04-03 03:34:13 +0200

    move catppuccin colors to common

--- home/polybar/default.nix
+++ home/polybar/default.nix
@@ -7,6 +7,11 @@ in {
   };
 
   config = mkIf cfg.enable {
+    xsession.windowManager.i3.config.startup = [{
+      command = "systemctl --user restart polybar.service";
+      always = true;
+    }];
+
     services.polybar = {
       enable = true;
       script = "polybar top &";
@@ -15,36 +20,7 @@ in {
         pulseSupport = true;
       };
 
-      config = let
-        colors = {
-          rosewater = "#f5e0dc";
-          flamingo = "#f2cdcd";
-          pink = "#f5c2e7";
-          mauve = "#cba6f7";
-          red = "#f38ba8";
-          maroon = "#eba0ac";
-          peach = "#fab387";
-          yellow = "#f9e2af";
-          green = "#a6e3a1";
-          teal = "#94e2d5";
-          sky = "#89dceb";
-          sapphire = "#74c7ec";
-          blue = "#89b4fa";
-          lavender = "#b4befe";
-          text = "#cdd6f4";
-          subtext1 = "#bac2de";
-          subtext0 = "#a6adc8";
-          overlay2 = "#9399b2";
-          overlay1 = "#7f849c";
-          overlay0 = "#6c7086";
-          surface2 = "#585b70";
-          surface1 = "#45475a";
-          surface0 = "#313244";
-          base = "#1e1e2e";
-          mantle = "#181825";
-          crust = "#11111b";
-          transparent = "#FF00000";
-        };
+      config = let colors = import ../../common/colors.nix;
       in {
         "bar/top" = {
           width = "100%";
