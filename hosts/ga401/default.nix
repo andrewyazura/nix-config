5cdb45015818b83242fa8e470e59a40f8d234ab0
Andrew Yatsura <andrewyazura203@proton.me>
2025-05-23 01:20:54 +0200

    remove gnome

--- hosts/ga401/default.nix
+++ hosts/ga401/default.nix
@@ -9,7 +9,7 @@
     audio.enable = true;
     fonts.enable = true;
     gaming.enable = true;
-    gnome.enable = true;
+    gnome.enable = false;
     i3.enable = true;
     networking.enable = true;
     nix.enable = true;
@@ -19,7 +19,7 @@
 
   home-manager.users.${username}.modules = {
     gnome = {
-      enable = true;
+      enable = false;
       enablePopShell = true;
     };
     i3.enable = true;
