d8d104b931017116c3966633e5425a538a5dcaf9
Andrew Yatsura <andrewyazura203@proton.me>
2025-08-28 22:25:29 +0200

    remove bhop config

--- home/cs2/default.nix
+++ home/cs2/default.nix
@@ -7,6 +7,7 @@ in {
   config = mkIf cfg.enable {
     home.file.".local/share/Steam/steamapps/common/Counter-Strike Global Offensive/game/csgo/cfg/autoexec.cfg".source =
       ./autoexec.cfg;
+
     # -refresh 144 -high -fullscreen -forcepreload 1 -threads 9 +mat_disable_fancy_blending 1 -mat_queue_mode 2 +engine_low_latency_sleep_after_client_tick
   };
 }
