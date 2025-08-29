ad33622ee639b6074bd9bb9746d5ff6284795dd0
Andrew Yatsura <andrewyazura203@proton.me>
2025-08-30 00:30:25 +0200

    increase terminal font size

--- home/ghostty/default.nix
+++ home/ghostty/default.nix
@@ -11,7 +11,7 @@ in {
       enable = true;
       settings = {
         background-opacity = 1;
-        font-size = 11;
+        font-size = 12;
         font-family = "FiraCode Nerd Font Mono";
         theme = "catppuccin-mocha";
         window-decoration = "none";
