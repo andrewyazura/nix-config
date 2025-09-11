94a9f8cfe0912e96d04fa68c10fb9d1e922c2c88
Andrew Yatsura <andrewyazura203@proton.me>
2025-09-11 16:59:20 +0200

    possible fix for sops

--- hosts/hetzner/default.nix
+++ hosts/hetzner/default.nix
@@ -38,8 +38,9 @@
   programs.dconf.enable = true;
 
   sops.secrets."duty-reminder.env" = {
-    sopsFile = "../../secrets/duty-reminder.env";
+    sopsFile = ../../secrets/duty-reminder.env;
     format = "binary";
+
     owner = "duty-reminder";
     group = "duty-reminder";
     mode = "0400";
