f73b82e8d5946634885533a422bcd933187b75ee
Andrew Yatsura <andrewyazura203@proton.me>
2025-10-11 05:26:49 +0200

    update ssh key file names

--- users/andrew/home/yorha2b/default.nix
+++ users/andrew/home/yorha2b/default.nix
@@ -1,6 +1,6 @@
 {
   programs.ssh.extraConfig = ''
-    IdentityFile ~/.ssh/id_ed25519_yorha2b_github_2509
-    IdentityFile ~/.ssh/id_ed25519_yorha2b_bunker_2509
+    IdentityFile ~/.ssh/id_ed25519_yorha2b_github_1110
+    IdentityFile ~/.ssh/id_ed25519_yorha2b_bunker_1110
   '';
 }
