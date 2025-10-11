4aa99767e610578c74f513b27181dba01ad5258f
Andrew Yatsura <andrewyazura203@proton.me>
2025-10-11 05:34:06 +0200

    add yorha2b ssh key to bunker

--- users/andrew/system/bunker/default.nix
+++ users/andrew/system/bunker/default.nix
@@ -1,6 +1,7 @@
 { username, ... }: {
   users.users.${username} = {
     openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICdMxw11eBmyXj7s+oFYx9Jpg7dTaDy0qb4STBXoeAH/ andrew@yorha2b"
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAdUlLMeczNPLgGu/4js6yGMdW1JCr2yUXKUGQUSKxNH andrew@yorha9s"
     ];
   };
