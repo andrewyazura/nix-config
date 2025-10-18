a3ee88ddae957391beb7c8924832f92ca8e85158
Andrew Yatsura <andrewyazura203@proton.me>
2025-10-18 13:08:38 +0200

    add fabric-api and distanthorizons to bombas mc server

--- system/minecraft-server/default.nix
+++ system/minecraft-server/default.nix
@@ -93,10 +93,26 @@ in {
           };
         };
         throwaway = template // {
+          symlinks = {
+            mods = pkgs.linkFarmFromDrvs "mods" (attrValues {
+              Fabric-API = pkgs.fetchurl {
+                url =
+                  "https://cdn.modrinth.com/data/P7dR8mSH/versions/lxeiLRwe/fabric-api-0.136.0%2B1.21.10.jar";
+                sha512 =
+                  "d6ad5afeb57dc6dbe17a948990fc8441fbbc13a748814a71566404d919384df8bd7abebda52a58a41eb66370a86b8c4f910b64733b135946ecd47e53271310b5";
+              };
+              DistantHorizons = pkgs.fetchurl {
+                url =
+                  "https://cdn.modrinth.com/data/uCdwusMi/versions/9Y10ZuWP/DistantHorizons-2.3.6-b-1.21.10-fabric-neoforge.jar";
+                sha512 =
+                  "1b1b70b7ec6290d152a5f9fa3f2e68ea7895f407c561b56e91aba3fdadef277cd259879676198d6481dcc76a226ff1aa857c01ae9c41be3e963b59546074a1fc";
+              };
+            });
+          };
           serverProperties = template.serverProperties // {
             motd = "\\u00A7b8 let dambili\\u00A7r";
             server-port = 25567;
-            spawn-protection=0;
+            spawn-protection = 0;
           };
         };
       };
