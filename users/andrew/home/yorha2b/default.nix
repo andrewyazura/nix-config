9bcf4baa0f5b26ec01e2a0368c742105101c0ec5
Andrew Yatsura <andrewyazura203@proton.me>
2025-10-18 05:25:21 +0200

    add bw-unlock zsh func

--- users/andrew/home/yorha2b/default.nix
+++ users/andrew/home/yorha2b/default.nix
@@ -1,9 +1,16 @@
 { config, ... }: {
   sops = {
     age.sshKeyPaths = [ "/home/andrew/.ssh/id_ed25519_yorha2b_nixconfig_1510" ];
-    secrets.ssh-config = {
-      sopsFile = ../../../../secrets/ssh-config;
-      format = "binary";
+    secrets = {
+      bw-password = {
+        sopsFile = ../../../../secrets/bw-password;
+        format = "binary";
+      };
+
+      ssh-config = {
+        sopsFile = ../../../../secrets/ssh-config;
+        format = "binary";
+      };
     };
   };
 
@@ -29,6 +36,13 @@
       }];
     };
 
-    zsh.shellAliases = { copy = "wl-copy"; };
+    zsh = {
+      initContent = ''
+        bw-unlock() {
+          export BW_SESSION=$(bw unlock --raw --passwordfile ${config.sops.secrets.bw-password.path})
+        }
+      '';
+      shellAliases = { copy = "wl-copy"; };
+    };
   };
 }
--- users/andrew/home/yorha9s/default.nix
+++ users/andrew/home/yorha9s/default.nix
@@ -1,9 +1,16 @@
 { config, ... }: {
   sops = {
     age.sshKeyPaths = [ "/home/andrew/.ssh/id_ed25519_yorha9s_nixconfig_1510" ];
-    secrets.ssh-config = {
-      sopsFile = ../../../../secrets/ssh-config;
-      format = "binary";
+    secrets = {
+      bw-password = {
+        sopsFile = ../../../../secrets/bw-password;
+        format = "binary";
+      };
+
+      ssh-config = {
+        sopsFile = ../../../../secrets/ssh-config;
+        format = "binary";
+      };
     };
   };
 
@@ -29,6 +36,13 @@
       }];
     };
 
-    zsh.shellAliases = { copy = "xclip -selection clipboard"; };
+    zsh = {
+      initContent = ''
+        bw-unlock() {
+          export BW_SESSION=$(bw unlock --raw --passwordfile ${config.sops.secrets.bw-password.path})
+        }
+      '';
+      shellAliases = { copy = "xclip -selection clipboard"; };
+    };
   };
 }
