535c42284aabba2c791101c151a9272d14a478ec
Andrew Yatsura <andrewyazura203@proton.me>
2025-10-18 12:29:06 +0200

    update minecraft settings

--- system/minecraft-server/default.nix
+++ system/minecraft-server/default.nix
@@ -2,13 +2,50 @@
 with lib;
 let
   cfg = config.modules.minecraft-server;
-  playerlist = [{
-    name = "andrewyazura";
-    uuid = "c2eb76fe-7bea-3498-b396-066ba66e08ae"; # offline user
-    # uuid = "2629aa63-faec-4548-bdc0-260850cf3cbf"; # online user
-    level = 4;
-    bypassesPlayerLimit = true;
-  }];
+  playerlist = [
+    {
+      name = "andrewyazura";
+      uuid = "c2eb76fe-7bea-3498-b396-066ba66e08ae"; # offline user
+      # uuid = "2629aa63-faec-4548-bdc0-260850cf3cbf"; # online user
+      level = 4;
+      bypassesPlayerLimit = false;
+    }
+    {
+      name = "boober";
+      uuid = "ecff3058-99c4-3f02-b7e5-154740f59632"; # offline user
+      # uuid = "ea38893c-11c9-493d-a285-69916f41f03f"; # online user
+      level = 4;
+      bypassesPlayerLimit = false;
+    }
+    {
+      name = "chief";
+      uuid = "adfadefe-d090-35d2-b71e-b7ed34dfca93"; # offline user
+      # uuid = "5aa86e15-6ddd-404e-93bf-71a87f448abc"; # online user
+      level = 0;
+      bypassesPlayerLimit = false;
+    }
+    {
+      name = "Singualrity";
+      uuid = "72271d99-4491-320e-8c32-bf4791c3f384"; # offline user
+      # uuid = "d2988641-9838-4fcd-ba0c-0b263205563a"; # online user
+      level = 0;
+      bypassesPlayerLimit = false;
+    }
+    {
+      name = "Sliparick";
+      uuid = "6def2816-d21e-35fa-bb5c-e989bed75acb"; # offline user
+      # uuid = "fec0904f-37d5-4e9f-8edc-55c3ed9cdfdf"; # online user
+      level = 0;
+      bypassesPlayerLimit = false;
+    }
+    {
+      name = "War_of_Lord";
+      uuid = "844cb079-11a2-3d18-bc9b-b15a7e1f1751"; # offline user
+      # uuid = "3da44920-a9d9-4925-b39c-5d47dfc6051e"; # online user
+      level = 0;
+      bypassesPlayerLimit = false;
+    }
+  ];
 in {
   imports = [ inputs.nix-minecraft.nixosModules.minecraft-servers ];
 
@@ -29,9 +66,9 @@ in {
           enable = true;
           package = pkgs.fabricServers.fabric-1_21_10;
 
-          operators = lib.listToAttrs (map (p: {
+          operators = listToAttrs (map (p: {
             name = p.name;
-            value = lib.removeAttrs p [ "name" ];
+            value = removeAttrs p [ "name" ];
           }) (filter (p: p.level > 0) playerlist));
 
           whitelist = listToAttrs (map (p: {
@@ -43,8 +80,7 @@ in {
             allow-cheats = false;
             difficulty = "normal";
             gamemode = "survival";
-            max-players = 8;
-            motd = "\\u00A7b8 let dambili\\u00A7r";
+            max-players = length playerlist;
             online-mode = false;
             white-list = true;
           };
@@ -52,14 +88,15 @@ in {
       in {
         main = template // {
           serverProperties = template.serverProperties // {
+            motd = "a nixos server";
             server-port = 25566;
           };
         };
         throwaway = template // {
           serverProperties = template.serverProperties // {
-            level-seed = "-8393641919666317235";
+            motd = "\\u00A7b8 let dambili\\u00A7r";
             server-port = 25567;
-            white-list = false;
+            spawn-protection=0;
           };
         };
       };
