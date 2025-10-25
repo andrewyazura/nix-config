eb2cada3fbacd983df4819e0bf1d25848c6fe5ff
Andrew Yatsura <andrewyazura203@gmail.com>
2025-10-25 19:12:42 +0200

    fix ghostty ssh-terminfo

--- home/ghostty/default.nix
+++ home/ghostty/default.nix
@@ -9,10 +9,13 @@ in {
   config = mkIf cfg.enable {
     programs.ghostty = {
       enable = true;
+      enableZshIntegration = true;
+
       settings = {
         background-opacity = 0.9;
         font-family = "Adwaita Mono Nerd Font";
         font-size = 10;
+        shell-integration-features = [ "ssh-terminfo" "ssh-env" ];
         theme = "Catppuccin Mocha";
         window-decoration = "server";
         window-inherit-working-directory = false;
