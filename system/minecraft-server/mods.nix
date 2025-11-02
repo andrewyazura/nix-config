43aff8ca863e223b3655ee273fc884308a417a51
Andrew Yatsura <andrewyazura203@gmail.com>
2025-11-03 00:28:16 +0100

    regenerate mod hashes using nix-prefetch-url, add vanish and spark

--- system/minecraft-server/mods.nix
+++ system/minecraft-server/mods.nix
@@ -4,30 +4,42 @@ with pkgs; {
     url =
       "https://cdn.modrinth.com/data/P7dR8mSH/versions/lxeiLRwe/fabric-api-0.136.0%2B1.21.10.jar";
     sha512 =
-      "d6ad5afeb57dc6dbe17a948990fc8441fbbc13a748814a71566404d919384df8bd7abebda52a58a41eb66370a86b8c4f910b64733b135946ecd47e53271310b5";
+      "2si04r7adzd9v26b49knwv41f8lz33bm1q67dhylic2m9dxprxbvy2d70cxj134arqlm0a8lw9vrys1hky912clgbhxpikxnpz5mbfn";
   };
   "distant-horizons" = fetchurl {
     url =
       "https://cdn.modrinth.com/data/uCdwusMi/versions/9Y10ZuWP/DistantHorizons-2.3.6-b-1.21.10-fabric-neoforge.jar";
     sha512 =
-      "1b1b70b7ec6290d152a5f9fa3f2e68ea7895f407c561b56e91aba3fdadef277cd259879676198d6481dcc76a226ff1aa857c01ae9c41be3e963b59546074a1fc";
+      "3ya2x30aickp5iypr0rrbh1gj2smwbg49mcgp41cj6ijxlnhxcx4z17xynzv8xbj5pbaqf50zs9ay7ad0p3zyprlm9d3432xjvp06qv";
   };
   "skin-restorer" = fetchurl {
     url =
-      "https://cdn.modrinth.com/data/ghrZDhGW/versions/MKWfnXfO/skinrestorer-2.4.3%2B1.21.9-fabric.jar";
+      "https://cdn.modrinth.com/data/ghrZDhGW/versions/MKWfnXfO/skinrestorer-2.4.3+1.21.9-fabric.jar";
     sha512 =
-      "a377133467707b88834642660a3a42137acb8abfbf80dbca87508b701aa4aca3e9d1738ef3fc098627c760e6afdea32fcdf8a5835942d291ef0640f3ef3667c5";
+      "32nfdpgyd00dvwis915k0x5z36jz8yymzk61ir7hq4zrwwffg8yk8xclhd712shhz5dp05zpy5cnyhk88x0lrj28s1qhyvhcws16xx3";
   };
   "lithium" = fetchurl {
     url =
-      "https://cdn.modrinth.com/data/gvQqBUqZ/versions/oGKQMdyZ/lithium-fabric-0.20.0%2Bmc1.21.10.jar";
+      "https://cdn.modrinth.com/data/gvQqBUqZ/versions/oGKQMdyZ/lithium-fabric-0.20.0+mc1.21.10.jar";
     sha512 =
-      "755c0e0fc7f6f38ac4d936cc6023d1dce6ecfd8d6bdc2c544c2a3c3d6d04f0d85db53722a089fa8be72ae32fc127e87f5946793ba6e8b4f2c2962ed30d333ed2";
+      "393wcqdscp9dhpjnklacfvr8rcpzs17q4py6ap7igx8k8126ysmvn7h0inksg1a9ia2rp3bipyyrrnws4in1k1nv728mwznqw7hwp3m";
   };
   "very-many-players" = fetchurl {
     url =
-      "https://cdn.modrinth.com/data/wnEe9KBa/versions/ppncuwIK/vmp-fabric-mc1.21.10-0.2.0%2Bbeta.7.215-all.jar";
+      "https://cdn.modrinth.com/data/wnEe9KBa/versions/ppncuwIK/vmp-fabric-mc1.21.10-0.2.0+beta.7.215-all.jar";
     sha512 =
-      "4c8026c7a2ee1cfd6b4113565543f87874e5da755640123561371f7ef0bf8d86e0ea4aadd5f96ee527b3903db5b3f01c00cb8052f72e802cee2dd4f32830df59";
+      "1cxyc18yga2vvich0pgfll0rc01rw5knlyr1cr7wmpgkmdd9bmf11ldpzq7w7rpc4si4h2nfpdfax3qz11mamhk85mzs77flb3jd02c";
+  };
+  "spark" = fetchurl {
+    url =
+      "https://cdn.modrinth.com/data/l6YH9Als/versions/eqIoLvsF/spark-1.10.152-fabric.jar";
+    sha512 =
+      "1wxc10n6hgwdn1qfy19swnzhpz7h4pwjx53kgkpl5vgsb157l9365pagw9zivlbnrxhbiwbs65d0j8nbgsfhlkzaib8gnsb3vwrb4pr";
+  };
+  "vanish" = fetchurl {
+    url =
+      "https://cdn.modrinth.com/data/UL4bJFDY/versions/lyeuEsAD/vanish-1.6.1+1.21.10.jar";
+    sha512 =
+      "2np50kafqa641r488iidig8fnnpwwzf85zv01s8cqhll0s40v212y6w181r85rxr75y0cg158slzbq74ddpindl4q8010dgm1v08q2b";
   };
 }
