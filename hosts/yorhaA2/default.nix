d97a99491bbb284aa205ee03a3c995dc45cc7fb1
Andrew Yatsura <andrewyazura203@gmail.com>
2025-11-19 00:26:45 +0200

    remove more stuff from yorhaA2 config

--- hosts/yorhaA2/default.nix
+++ hosts/yorhaA2/default.nix
@@ -1,39 +1,15 @@
 { inputs, ... }: {
-  imports = [ inputs.private-config.darwinModules.default or { } ];
-
-  modules = {
-    fonts.enable = true;
-    nix.enable = true;
-    work.enable = true;
-
-    programs = {
-      enable = true;
-      enableMinecraft = true;
-    };
-
-    syncthing = {
-      enable = true;
-      username = "andrew";
-    };
-  };
+  modules = { fonts.enable = true; };
 
   home-manager.users.andrew = {
-    imports = [
-      ../../home
-      ../../users/andrew/home
-      ../../users/andrew/home/yorhaA2
-
-      inputs.private-config.homeManagerModules.default
-    ];
+    imports =
+      [ ../../home ../../users/andrew/home ../../users/andrew/home/yorhaA2 ];
   };
 
   homebrew = {
     enable = true;
-
     onActivation = { autoUpdate = false; };
   };
-
-  time.timeZone = "Europe/Kyiv";
 
   security.pam.services.sudo_local.touchIdAuth = true;
 
@@ -47,4 +23,6 @@
       };
     };
   };
+
+  time.timeZone = "Europe/Kyiv";
 }
