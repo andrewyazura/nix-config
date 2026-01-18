4983dfebdac03c89c08cc07801cb1c71d2c0dd2e
Andrew Yatsura <andrewyazura203@gmail.com>
2026-01-18 02:37:11 +0200

    add yorha9s keys for new bunker

--- users/andrew/system/bunker/default.nix
+++ users/andrew/system/bunker/default.nix
@@ -1,6 +1,7 @@
 {
   users.users.andrew = {
     openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIw3NHKUDHnahzTI3LVNQMlypTJV9oBVwESBKBBk9vJb andrew@yorha9s"
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIANFbNJgCaS7BXFvCZGKX1AHGeVpjlkXMoSr0TT1fkm/ andrew@yorhaA2"
     ];
   };
