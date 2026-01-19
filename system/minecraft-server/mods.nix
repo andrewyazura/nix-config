18fbb968f42943011110420f457fc355c4aa5959
Andrew Yatsura <andrewyazura203@gmail.com>
2026-01-19 18:07:16 +0100

    remove distant horizons from mc server

--- system/minecraft-server/mods.nix
+++ system/minecraft-server/mods.nix
@@ -5,10 +5,6 @@ with pkgs;
     url = "https://cdn.modrinth.com/data/P7dR8mSH/versions/DdVHbeR1/fabric-api-0.141.1+1.21.11.jar";
     sha512 = "0pk785blm80s8549g0v3ha2j65vpz3q8fy0gfkblvxj844kpxkh78mq2rhy4zmw7yx89hj2yh6635kidh6wk669vwwk14i2pvf4anqq";
   };
-  "distant-horizons" = fetchurl {
-    url = "https://cdn.modrinth.com/data/uCdwusMi/versions/GT3Bm3GN/DistantHorizons-2.4.5-b-1.21.11-fabric-neoforge.jar";
-    sha512 = "3hnbwfpqawp6z5brlwxvwfv3iinrs0rsmqs0mdr63ffp23w8lxg22z7inpm764pr6rna6zgpfr5x8gi4vkwns2176vm9xgnq7x77xm9";
-  };
   "skin-restorer" = fetchurl {
     url = "https://cdn.modrinth.com/data/ghrZDhGW/versions/8K5Fuf9S/skinrestorer-2.4.3+1.21.11-fabric.jar";
     sha512 = "1wv85zxwbm61xmimjf1zxh25cldqir3ghyd08376mq144lvyix4z92wgb730m4nhliizhazxqkz0mrc316gbkcpj1lz2yz038y0868s";
