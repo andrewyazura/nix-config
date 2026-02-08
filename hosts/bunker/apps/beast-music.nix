ace1ae4e3cb292d883f444127b4f00a328365a6c
Andrew Yatsura <andrewyazura203@gmail.com>
2026-02-08 23:52:13 +0100

    update beast-music

--- hosts/bunker/apps/beast-music.nix
+++ hosts/bunker/apps/beast-music.nix
@@ -1,5 +1,6 @@
 {
   inputs,
+  config,
   ...
 }:
 {
@@ -9,5 +10,15 @@
     enable = true;
     envFile = "/var/lib/beastmusic/.env";
     dataDir = "/var/lib/beastmusic";
+  };
+
+  services.nginx.virtualHosts."lelomed.andrewyazura.com" = {
+    forceSSL = true;
+    sslCertificate = config.sops.secrets."andrewyazura.crt".path;
+    sslCertificateKey = config.sops.secrets."andrewyazura.key".path;
+
+    locations."/" = {
+      proxyPass = "http://127.0.0.1:5000";
+    };
   };
 }
