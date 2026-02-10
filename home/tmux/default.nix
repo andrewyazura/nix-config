4b1957adabd13366c61525765bd9ec5b4265a516
Andrew Yatsura <andrewyazura203@gmail.com>
2026-02-10 18:54:41 +0100

    update tmux status bar colors

--- home/tmux/default.nix
+++ home/tmux/default.nix
@@ -23,27 +23,33 @@ in
       keyMode = "vi";
       mouse = true;
 
-      extraConfig = ''
-        set -g default-command "${pkgs.zsh}/bin/zsh --login"
-        set -g default-shell "${pkgs.zsh}/bin/zsh"
+      extraConfig =
+        let
+          palette = import ../../common/colors.nix;
+        in
+        ''
+          set -g default-command "${pkgs.zsh}/bin/zsh --login"
+          set -g default-shell "${pkgs.zsh}/bin/zsh"
 
-        set -g default-terminal "tmux-256color"
-        set -ag terminal-overrides ",xterm-256color:RGB"
+          set -g default-terminal "tmux-256color"
+          set -ag terminal-overrides ",xterm-256color:RGB"
 
-        set -g status-left-length 50
-        set -g status-left "[#{session_name}] "
+          set -g status-style fg=${palette.base},bg=${palette.mauve}
 
-        set -g status-right-length 150
-        set -g status-right "#{?window_bigger,[#{window_offset_x}#,#{window_offset_y}] ,}\"#{=21:pane_title}\" %H:%M %d-%b-%y"
+          set -g status-left-length 50
+          set -g status-left "[#{session_name}] "
 
-        unbind C-b
-        set -g prefix C-Space
-        bind C-Space send-prefix
+          set -g status-right-length 150
+          set -g status-right "#{?window_bigger,[#{window_offset_x}#,#{window_offset_y}] ,}\"#{=21:pane_title}\" %A, %B %d %H:%M"
 
-        bind -T copy-mode-vi v send-keys -X begin-selection
-        bind -T copy-mode-vi y send-keys -X copy-pipe-and-cancel
-        bind -T copy-mode-vi s send-keys -X rectangle-toggle
-      '';
+          unbind C-b
+          set -g prefix C-Space
+          bind C-Space send-prefix
+
+          bind -T copy-mode-vi v send-keys -X begin-selection
+          bind -T copy-mode-vi y send-keys -X copy-pipe-and-cancel
+          bind -T copy-mode-vi s send-keys -X rectangle-toggle
+        '';
     };
   };
 }
