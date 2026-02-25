d52e8c5b01bedaf05127ce7dcf1767037c54b150
Andrew Yatsura <andrewyazura203@gmail.com>
2026-02-25 01:30:45 +0100

    add autopairs and surround for parentheses

--- home/neovim/configs/cmp.lua
+++ home/neovim/configs/cmp.lua
@@ -45,3 +45,6 @@ cmp.setup.cmdline(":", {
 	}),
 	matching = { disallow_symbol_nonprefix_matching = false },
 })
+
+local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
