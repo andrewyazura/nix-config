4a5ed944b8f5e502d87748a254689754129008d9
Andrew Yatsura <andrewyazura203@gmail.com>
2026-02-25 01:32:56 +0100

    add formatter for kotlin

--- home/neovim/configs/conform.lua
+++ home/neovim/configs/conform.lua
@@ -2,13 +2,16 @@ local conform = require("conform")
 
 conform.setup({
 	formatters_by_ft = {
+		javascript = { "prettier" },
+		json = { "jq" },
+		kotlin = { "ktfmt" },
 		lua = { "stylua" },
 		nix = { "nixfmt" },
 		python = { "ruff_organize_imports", "ruff_format" },
-		json = { "jq" },
+		typescript = { "prettier" },
 	},
 })
 
 vim.keymap.set("n", "<leader>F", function()
 	conform.format({ async = true, lsp_format = "fallback" })
-end)
+end, { desc = "Format buffer" })
