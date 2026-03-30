9e44facf88c0299579ebd2f9341776276953df85
Andrew Yatsura <andrewyazura203@gmail.com>
2026-03-31 01:07:12 +0300

    remove launchd agent

--- darwin/system-defaults/default.nix
+++ darwin/system-defaults/default.nix
@@ -56,59 +56,5 @@ in
         };
       };
     };
-
-    launchd.user.agents =
-      let
-        escape = "0x700000029";
-        capsLock = "0x700000039";
-
-        fn = "0xFF00000003";
-        leftCtrl = "0x7000000E0";
-      in
-      {
-        KeyMappingsBuiltin = {
-          serviceConfig = {
-            Label = "com.local.KeyMappingsBuiltin";
-            RunAtLoad = true;
-            LaunchEvents = {
-              "com.apple.notifyd.matching" = {
-                "com.local.KeyMappingsBuiltin.wake" = {
-                  Notification = "com.apple.screenIsUnlocked";
-                };
-              };
-            };
-
-            ProgramArguments = [
-              "/usr/bin/hidutil"
-              "property"
-              "--matching"
-              ''{"Built-In": true}''
-              "--set"
-              ''
-                {"UserKeyMapping":[
-                    {
-                      "HIDKeyboardModifierMappingSrc": ${escape},
-                      "HIDKeyboardModifierMappingDst": ${capsLock}
-                    },
-
-                    {
-                      "HIDKeyboardModifierMappingSrc": ${capsLock},
-                      "HIDKeyboardModifierMappingDst": ${escape}
-                    },
-
-                    {
-                      "HIDKeyboardModifierMappingSrc": ${fn},
-                      "HIDKeyboardModifierMappingDst": ${leftCtrl}
-                    },
-
-                    {
-                      "HIDKeyboardModifierMappingSrc": ${leftCtrl},
-                      "HIDKeyboardModifierMappingDst": ${fn}
-                    }
-                ]}''
-            ];
-          };
-        };
-      };
   };
 }
