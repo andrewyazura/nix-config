584e56bf8993345f4cf1542df3d422d020599156
Andrew Yatsura <andrewyazura203@gmail.com>
2026-05-08 12:29:36 +0200

    remove unused functions from neovim config

--- home/neovim/default.nix
+++ home/neovim/default.nix
@@ -36,210 +36,197 @@ in
       tree-sitter
     ];
 
-    programs.neovim =
-      let
-        toLua = str: ''
-          lua << EOF
-          ${str}
-          EOF
-        '';
-        toLuaFile = path: ''
-          lua << EOF
-          ${builtins.readFile path}
-          EOF
-        '';
-      in
-      {
-        enable = true;
-        defaultEditor = true;
+    programs.neovim = {
+      enable = true;
+      defaultEditor = true;
 
-        viAlias = true;
-        vimAlias = true;
-        vimdiffAlias = true;
+      viAlias = true;
+      vimAlias = true;
+      vimdiffAlias = true;
 
-        withRuby = false;
-        withPython3 = false;
+      withRuby = false;
+      withPython3 = false;
 
-        extraPackages = with pkgs; [
-          # tools
-          fd
-          ripgrep
-          tree-sitter
+      extraPackages = with pkgs; [
+        # tools
+        fd
+        ripgrep
+        tree-sitter
 
-          # formatters
-          ktfmt
-          nixfmt
-          prettier
-          ruff
-          stylua
+        # formatters
+        ktfmt
+        nixfmt
+        prettier
+        ruff
+        stylua
 
-          # lsp servers
-          lua-language-server
-          nil
-          ty
-          typescript-language-server
-          zls
-        ];
+        # lsp servers
+        lua-language-server
+        nil
+        ty
+        typescript-language-server
+        zls
+      ];
 
-        plugins = with pkgs.vimPlugins; [
-          {
-            plugin = catppuccin-nvim;
-            type = "lua";
-            config = toLuaFile ./configs/options.lua;
-            # this hack sets leader key before any other hotkeys are added
-          }
+      plugins = with pkgs.vimPlugins; [
+        {
+          plugin = catppuccin-nvim;
+          type = "lua";
+          config = builtins.readFile ./configs/options.lua;
+          # this hack sets leader key before any other hotkeys are added
+        }
 
-          {
-            plugin = conform-nvim;
-            type = "lua";
-            config = toLuaFile ./configs/conform.lua;
-          }
+        {
+          plugin = conform-nvim;
+          type = "lua";
+          config = builtins.readFile ./configs/conform.lua;
+        }
 
-          diffview-nvim
-          nvim-web-devicons # for diffview-nvim
+        diffview-nvim
+        nvim-web-devicons # for diffview-nvim
 
-          {
-            plugin = flash-nvim;
-            type = "lua";
-            config = toLuaFile ./configs/flash.lua;
-          }
+        {
+          plugin = flash-nvim;
+          type = "lua";
+          config = builtins.readFile ./configs/flash.lua;
+        }
 
-          {
-            plugin = fzf-lua;
-            type = "lua";
-            config = toLuaFile ./configs/fzf.lua;
-          }
+        {
+          plugin = fzf-lua;
+          type = "lua";
+          config = builtins.readFile ./configs/fzf.lua;
+        }
 
-          {
-            plugin = gitsigns-nvim;
-            type = "lua";
-            config = toLuaFile ./configs/gitsigns.lua;
-          }
+        {
+          plugin = gitsigns-nvim;
+          type = "lua";
+          config = builtins.readFile ./configs/gitsigns.lua;
+        }
 
-          hardtime-nvim
-          markdown-preview-nvim
-          mini-icons
+        hardtime-nvim
+        markdown-preview-nvim
+        mini-icons
 
-          {
-            plugin = neotest;
-            type = "lua";
-            config = toLuaFile ./configs/neotest.lua;
-          }
-          neotest-python # for neotest
-          neotest-gradle # for neotest
+        {
+          plugin = neotest;
+          type = "lua";
+          config = builtins.readFile ./configs/neotest.lua;
+        }
+        neotest-python # for neotest
+        neotest-gradle # for neotest
 
-          {
-            plugin = nvim-autopairs;
-            type = "lua";
-            config = toLuaFile ./configs/autopairs.lua;
-          }
+        {
+          plugin = nvim-autopairs;
+          type = "lua";
+          config = builtins.readFile ./configs/autopairs.lua;
+        }
 
-          {
-            plugin = nvim-cmp;
-            type = "lua";
-            config = toLuaFile ./configs/cmp.lua;
-          }
-          cmp-buffer # for nvim-cmp
-          cmp-cmdline # for nvim-cmp
-          cmp-nvim-lsp # for nvim-cmp
-          cmp-nvim-lsp-document-symbol # for nvim-cmp
-          cmp-nvim-lsp-signature-help # for nvim-cmp
-          cmp-path # for nvim-cmp
+        {
+          plugin = nvim-cmp;
+          type = "lua";
+          config = builtins.readFile ./configs/cmp.lua;
+        }
+        cmp-buffer # for nvim-cmp
+        cmp-cmdline # for nvim-cmp
+        cmp-nvim-lsp # for nvim-cmp
+        cmp-nvim-lsp-document-symbol # for nvim-cmp
+        cmp-nvim-lsp-signature-help # for nvim-cmp
+        cmp-path # for nvim-cmp
 
-          {
-            plugin = nvim-dap;
-            type = "lua";
-            config = toLuaFile ./configs/dap.lua;
-          }
-          nvim-dap-ui # for nvim-dap
-          nvim-dap-virtual-text # for nvim-dap
-          nvim-nio # for nvim-dap-ui
+        {
+          plugin = nvim-dap;
+          type = "lua";
+          config = builtins.readFile ./configs/dap.lua;
+        }
+        nvim-dap-ui # for nvim-dap
+        nvim-dap-virtual-text # for nvim-dap
+        nvim-nio # for nvim-dap-ui
 
-          {
-            plugin = nvim-surround;
-            type = "lua";
-            config = toLuaFile ./configs/surround.lua;
-          }
+        {
+          plugin = nvim-surround;
+          type = "lua";
+          config = builtins.readFile ./configs/surround.lua;
+        }
 
-          {
-            plugin = obsidian-nvim;
-            type = "lua";
-            config = toLua ''
-              ${builtins.readFile ./configs/obsidian.lua}
-              setup_obsidian("${cfg.vaultPath}")
-            '';
-          }
-          plenary-nvim # for obsidian-nvim
+        {
+          plugin = obsidian-nvim;
+          type = "lua";
+          config = ''
+            ${builtins.readFile ./configs/obsidian.lua}
+            setup_obsidian("${cfg.vaultPath}")
+          '';
+        }
+        plenary-nvim # for obsidian-nvim
 
-          {
-            plugin = nvim-treesitter.withPlugins (p: [
-              p.bash
-              p.css
-              p.dockerfile
-              p.git_rebase
-              p.gitcommit
-              p.go
-              p.html
-              p.java
-              p.javascript
-              p.json
-              p.kotlin
-              p.lua
-              p.markdown
-              p.markdown_inline
-              p.nix
-              p.python
-              p.regex
-              p.rust
-              p.sql
-              p.toml
-              p.typescript
-              p.vimdoc
-              p.yaml
-              p.zig
-            ]);
-            type = "lua";
-            config = toLuaFile ./configs/treesitter.lua;
-          }
-          nvim-treesitter-context # for nvim-treesitter
-          nvim-treesitter-textobjects # for nvim-treesitter
+        {
+          plugin = nvim-treesitter.withPlugins (p: [
+            p.bash
+            p.css
+            p.dockerfile
+            p.git_rebase
+            p.gitcommit
+            p.go
+            p.html
+            p.java
+            p.javascript
+            p.json
+            p.kotlin
+            p.lua
+            p.markdown
+            p.markdown_inline
+            p.nix
+            p.python
+            p.regex
+            p.rust
+            p.sql
+            p.toml
+            p.typescript
+            p.vimdoc
+            p.yaml
+            p.zig
+          ]);
+          type = "lua";
+          config = builtins.readFile ./configs/treesitter.lua;
+        }
+        nvim-treesitter-context # for nvim-treesitter
+        nvim-treesitter-textobjects # for nvim-treesitter
 
-          {
-            plugin = oil-nvim;
-            type = "lua";
-            config = toLuaFile ./configs/oil.lua;
-          }
+        {
+          plugin = oil-nvim;
+          type = "lua";
+          config = builtins.readFile ./configs/oil.lua;
+        }
 
-          {
-            plugin = persistence-nvim;
-            type = "lua";
-            config = toLuaFile ./configs/persistence.lua;
-          }
+        {
+          plugin = persistence-nvim;
+          type = "lua";
+          config = builtins.readFile ./configs/persistence.lua;
+        }
 
-          {
-            plugin = todo-comments-nvim;
-            type = "lua";
-            config = toLuaFile ./configs/todo-comments.lua;
-          }
+        {
+          plugin = todo-comments-nvim;
+          type = "lua";
+          config = builtins.readFile ./configs/todo-comments.lua;
+        }
 
-          {
-            plugin = trouble-nvim;
-            type = "lua";
-            config = toLuaFile ./configs/trouble.lua;
-          }
+        {
+          plugin = trouble-nvim;
+          type = "lua";
+          config = builtins.readFile ./configs/trouble.lua;
+        }
 
-          {
-            plugin = undotree;
-            type = "lua";
-            config = toLuaFile ./configs/undotree.lua;
-          }
+        {
+          plugin = undotree;
+          type = "lua";
+          config = builtins.readFile ./configs/undotree.lua;
+        }
 
-          {
-            plugin = which-key-nvim;
-            type = "lua";
-            config = toLuaFile ./configs/which-key.lua;
-          }
-        ];
-      };
+        {
+          plugin = which-key-nvim;
+          type = "lua";
+          config = builtins.readFile ./configs/which-key.lua;
+        }
+      ];
+    };
   };
 }
