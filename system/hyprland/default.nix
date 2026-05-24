5da3c47c6f92784aba3f95f690f671774759817e
Andrew Yatsura <andrewyazura203@gmail.com>
2026-05-24 22:47:25 +0200

    add greetd to hyprland

--- system/hyprland/default.nix
+++ system/hyprland/default.nix
@@ -22,5 +22,29 @@ in
       package = hyprlandPkgs.hyprland;
       portalPackage = hyprlandPkgs.xdg-desktop-portal-hyprland;
     };
+
+    xdg.portal = {
+      enable = true;
+      extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
+      config.common.default = [ "gtk" ];
+    };
+
+    services = {
+      playerctld.enable = true;
+
+      greetd = {
+        enable = true;
+        settings = {
+          default_session = {
+            command = "${pkgs.tuigreet}/bin/tuigreet -r --time --cmd Hyprland";
+          };
+        };
+      };
+    };
+
+    systemd.services.greetd = {
+      serviceConfig.Type = "idle";
+      unitConfig.After = mkForce [ "multi-user.target" ];
+    };
   };
 }
