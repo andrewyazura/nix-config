ebf743006b6ea5b98a2ec26253d46cd9ea242924
Andrew Yatsura <andrewyazura203@gmail.com>
2026-06-24 16:34:47 +0200

    add yaroslave to cs2 server admins

--- hosts/bunker/default.nix
+++ hosts/bunker/default.nix
@@ -48,6 +48,10 @@
               identity = "76561198341773564";
               flags = [ "@css/root" ];
             };
+            "yaroslave" = {
+              identity = "76561198028746163";
+              flags = [ "@css/root" ];
+            };
           };
           environmentFiles = [
             config.sops.secrets.cs2-env.path
